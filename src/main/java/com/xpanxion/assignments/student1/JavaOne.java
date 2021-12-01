412b65b13311988e789139add1b170ca55c0a68b
Kris Norful <krisnorful@gmail.com>
2021-12-01 14:22:41 -0600

    Finished ex. 2

--- src/main/java/com/xpanxion/assignments/student1/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student1/JavaOne.java
@@ -24,12 +24,30 @@ public class JavaOne {
 
         //return name in upper case
         System.out.println("Your name in upper case is: " + userName.toUpperCase());
-//        System.out.println("Student 1: ex1.");
 
     }
 
     public void ex2() {
-        System.out.println("Student 1: ex2.");
+        //prompt user to enter a string
+        System.out.print("Enter a string: ");
+
+        //read string input from user
+        Scanner scanner = new Scanner(System.in);
+        String userString = scanner.nextLine();
+
+        //create count variable
+        int capitalLetters = 0;
+
+        //loop string and count amount of capital letters
+        for (int i = 0; i < userString.length(); i++) {
+            char currentChar = userString.charAt(i);
+            if (Character.isUpperCase(currentChar)) {
+                capitalLetters++;
+            }
+        }
+        //return count of capital letters
+        System.out.println("Number of uppercase letters: " + capitalLetters);
+
     }
 
     public void ex3() {
