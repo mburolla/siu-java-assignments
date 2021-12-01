76cc49ff9eb01c421b8bda6fc662e37c9a28f8d4
Meg Goodwin <m0g0gqc@homeoffice.wal-mart.com>
2021-12-01 16:19:33 -0600

    Finished ex.6

--- src/main/java/com/xpanxion/assignments/student8/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student8/JavaOne.java
@@ -97,6 +97,13 @@ public class JavaOne {
     }
 
     public void ex6() {
+        System.out.println("Student 8: ex5.");
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        int result = a + b;
+        System.out.println(String.format("Result: %s", result));
 
     }
     //
