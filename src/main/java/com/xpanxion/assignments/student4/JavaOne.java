2bae7ba0f707a755a954867e5796fbcc25bfdfba
Elizabeth Michael <lisakada@gmail.com>
2021-12-01 14:54:56 -0600

    Finished exercise 1

--- src/main/java/com/xpanxion/assignments/student4/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student4/JavaOne.java
@@ -1,5 +1,8 @@
 package com.xpanxion.assignments.student4;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class JavaOne {
 
     //
@@ -13,8 +16,12 @@ public class JavaOne {
     //
 
     public void ex1() {
-        System.out.println("Student 4: ex1.");
-    }
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("Enter your name :");
+            String name=scanner.nextLine().toUpperCase();
+            System.out.println("your name in uppercase is: "+ name);
+        }
+
 
     public void ex2() {
         System.out.println("Student 4: ex2.");
