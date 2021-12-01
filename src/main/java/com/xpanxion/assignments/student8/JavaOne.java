19ddc48fd849305f704e188468041a9083cdfb16
Meg Goodwin <m0g0gqc@homeoffice.wal-mart.com>
2021-12-01 15:47:49 -0600

    Finished ex.1, ex.2, ex.3, ex.4, and ex.5

--- src/main/java/com/xpanxion/assignments/student8/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student8/JavaOne.java
@@ -1,5 +1,9 @@
 package com.xpanxion.assignments.student8;
 
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class JavaOne {
 
     //
@@ -11,19 +15,92 @@ public class JavaOne {
     //
     // Public methods
     //
+    Scanner scanner = new Scanner(System.in);
+
 
     public void ex1() {
         System.out.println("Student 8: ex1.");
+//        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your name in upper case is: " + name.toUpperCase());
     }
 
     public void ex2() {
         System.out.println("Student 8: ex2.");
+        System.out.print("Enter a string: ");
+//        Scanner scanner = new Scanner(System.in);
+        String phraseToCountUppers = scanner.nextLine();
+
+        int numOfUppers = 0;
+        for (char c : phraseToCountUppers.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                numOfUppers++;
+            }
+        }
+        System.out.println("Number of uppercase letters: " + numOfUppers);
     }
 
     public void ex3() {
         System.out.println("Student 8: ex3.");
+        System.out.println("Enter a string: ");
+        String userPhrase = scanner.nextLine();
+        String[] userTokens = userPhrase.split(" ");
+
+        for (int i = 0; i < userTokens.length; i += 2) {
+            userTokens[i] = userTokens[i].toUpperCase();
+        }
+
+        String formattedPhrase = String.join(" ", userTokens);
+
+        System.out.println(formattedPhrase);
     }
 
+    public void ex4() {
+        System.out.println("Student 8: ex4.");
+        System.out.print("Enter string: ");
+        String userWord = scanner.next().toLowerCase();
+        StringBuilder sb = new StringBuilder(userWord);
+        sb = sb.reverse();
+        String reversedWord = sb.toString();
+
+
+        if (reversedWord.equals(userWord)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    public void ex5() {
+        System.out.println("Student 8: ex5.");
+        String userStr = "";
+
+        while(!userStr.toLowerCase().equals("quit")) {
+            System.out.print("Enter a string: ");
+            userStr = scanner.nextLine().toLowerCase();
+            if (userStr.equals("quit")) {
+                break;
+            } else {
+                int vowelCount = 0;
+                int consonantCount = 0;
+                for (int i = 0; i < userStr.length(); i++) {
+
+                    char c = userStr.charAt(i);
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                        vowelCount++;
+                    else if (c >= 'a' && c <= 'z')
+                        consonantCount++;
+                }
+                System.out.println("Number of vowels: " + vowelCount);
+                System.out.println("Number of consonants: " + consonantCount);
+            }
+        }
+    }
+
+    public void ex6() {
+
+    }
     //
     // Private helper methods
     //
