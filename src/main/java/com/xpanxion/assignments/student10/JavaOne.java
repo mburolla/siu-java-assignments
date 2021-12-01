42f21eac872347b0f7b533f9fd3fdfaa9a4df16e
Rachel Eckels <Rachel.Eckels@walmart.com>
2021-12-01 16:01:39 -0600

    Ex. 2 completed

--- src/main/java/com/xpanxion/assignments/student10/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student10/JavaOne.java
@@ -28,7 +28,20 @@ public class JavaOne {
     }
 
     public void ex2() {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Student 10: ex2.");
+        System.out.print("Enter a string: ");
+        String string = scanner.nextLine();
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            char letter = string.charAt(i);
+            if(Character.isUpperCase(letter)) {
+                count++;
+            }
+        }
+        System.out.print("Number of upper case letters: "+ count);
+
     }
 
     public void ex3() {
