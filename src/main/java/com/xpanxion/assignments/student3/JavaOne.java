597227e69579de2e453bcaae91577dbbfdad758c
Keith Inscore <keith.inscore@walmart.com>
2021-12-01 15:36:38 -0600

    Finished all excercises for Java1

--- src/main/java/com/xpanxion/assignments/student3/JavaOne.java
+++ src/main/java/com/xpanxion/assignments/student3/JavaOne.java
@@ -1,6 +1,7 @@
 package com.xpanxion.assignments.student3;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class JavaOne {
 
@@ -29,10 +30,47 @@ public class JavaOne {
 
     public void ex2() {
         System.out.println("Student 3: ex2.");
+
+        int upperCase = 0;
+        String string;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(("Enter a String: "));
+        string = scanner.nextLine();
+
+        for (int i = 0; i < string.length(); i++) {
+            char letter = string.charAt(i);
+            if (letter >= 'A' && letter <= 'Z')
+                upperCase++;
+        }
+
+        System.out.println("The number of uppercase letters is  " + upperCase);
+
+
     }
 
     public void ex3() {
         System.out.println("Student 3: ex3.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter a string: ");
+        String string = scanner.nextLine();
+        int i = 0;
+        var finalPhrase= " ";
+
+        StringTokenizer words = new StringTokenizer(string);
+
+        while (words.hasMoreTokens()){
+            if (i % 2 == 1){
+                finalPhrase += words.nextToken();
+            } else {
+                finalPhrase += words.nextToken().toUpperCase();
+            }
+            finalPhrase += " ";
+            i++;
+        }
+        System.out.println(finalPhrase);
     }
 
     //
