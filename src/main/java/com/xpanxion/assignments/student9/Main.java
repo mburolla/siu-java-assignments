ecdd649e317160b563186b9938a37dd6a4b6bb8e
EMANUEL GARCIA <e0g0186@HOMEOFFICE.wal-mart.com>
2021-12-07 14:16:06 -0600

    Update

--- src/main/java/com/xpanxion/assignments/student9/Main.java
+++ src/main/java/com/xpanxion/assignments/student9/Main.java
@@ -15,8 +15,8 @@ public class Main {
        // javaOne.ex10();
 
         var javaTwo = new JavaTwo();
-        //javaTwo.ex1();
-        javaTwo.ex2();
+        javaTwo.ex1();
+        //javaTwo.ex2();
 
     }
 }
--- src/main/java/com/xpanxion/assignments/student9/Person.java
+++ src/main/java/com/xpanxion/assignments/student9/Person.java
@@ -1,4 +1,42 @@
 package com.xpanxion.assignments.student9;
 
 public class Person {
+    int id;
+    String firstName;
+    String lastName;
+
+    public Person(int id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Person{").append("id=").append(id).append(", firstName='").append(firstName).append('\'').append(", lastName='").append(lastName).append('\'').append('}').toString();
+    }
 }
