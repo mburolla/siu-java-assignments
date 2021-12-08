cf7864d99658e848ee414bc9e689bf17d6518539
Aaron Weegens <Aaron.Weegens@walmart.com>
2021-12-08 11:55:13 -0600

    refactor auditorium array to ArrayList

--- src/main/java/com/xpanxion/assignments/student5/MovieTheater.java
+++ src/main/java/com/xpanxion/assignments/student5/MovieTheater.java
@@ -1,11 +1,14 @@
 package com.xpanxion.assignments.student5;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class MovieTheater {
     private double salesTotal;
-    private StringBuilder[] auditorium;
+    private List<StringBuilder> auditorium;
     private Scanner sc = new Scanner(System.in);
     private NumberFormat formatter = NumberFormat.getCurrencyInstance();
     private String emptySeat = "0";
@@ -38,28 +41,27 @@ public class MovieTheater {
     }
 
     private void buildTheater(int rows, int seats) {
-
-        auditorium = new StringBuilder[rows];
+        auditorium = new ArrayList<>();
 
         for (int i = 0; i < rows; i++) {
-            auditorium[i] = new StringBuilder(emptySeat.repeat(seats));
+            auditorium.add(new StringBuilder(emptySeat.repeat(seats)));
         }
     }
+
     private void sellTicket (String[] seatSold) {
         if (seatSold.length != 2) return;
         int row = Integer.valueOf(seatSold[0]);
         int seat = Integer.valueOf(seatSold[1]);
-        if (auditorium[row-1].charAt(seat-1)=='0') {
-            auditorium[row-1].setCharAt(seat-1, occupiedSeat.charAt(0));
+        if (auditorium.get(row-1).charAt(seat-1)=='0') {
+            auditorium.get(row-1).setCharAt(seat-1, occupiedSeat.charAt(0));
             salesTotal += row;
         } else System.out.println("That seat has already been sold.");
         printSeating();
         printSalesTotal();
     }
+
     private void printSeating() {
-        for (StringBuilder s: auditorium) {
-            System.out.println(s.toString());
-        }
+        auditorium.forEach(System.out::println);
     }
 
     private void printSalesTotal() {
