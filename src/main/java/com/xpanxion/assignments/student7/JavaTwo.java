b91b42b461d96f1a5dbbb2534dd8fef2d8abce22
Austin King <austintriesemail@gmail.com>
2021-12-08 16:00:19 -0600

    Finished ex9

--- src/main/java/com/xpanxion/assignments/student7/JavaTwo.java
+++ src/main/java/com/xpanxion/assignments/student7/JavaTwo.java
@@ -146,5 +146,22 @@ public class JavaTwo {
         }
     }
 
+    public void ex9() {
+        var personList = Arrays.asList(
+        new Person(1, "Charlie", "Jones"),
+                new Person(2, "Zoey", "Smith"),
+                new Person(3, "Adam", "Anderson")
+);
+
+        List<Person> filteredList = personList.stream()
+                .filter(p -> p.getLastName().equals("Smith"))
+                .collect(Collectors.toList());
+
+        for (Person p : filteredList) {
+            System.out.println(p);
+        }
+
+    }
+
 
 }
