035075749b7de58a57edc6ff1845aa8a0fa63e07
EMANUEL GARCIA <e0g0186@HOMEOFFICE.wal-mart.com>
2021-12-09 15:19:03 -0600

    Update

---
+++ src/main/java/com/xpanxion/assignments/student9/CalculatorException.java
@@ -0,0 +1,8 @@
+package com.xpanxion.assignments.student9;
+
+public class CalculatorException extends Exception{
+
+    public CalculatorException(String message){
+        super(message);
+    }
+}
