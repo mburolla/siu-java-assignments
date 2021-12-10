f04e71bfebb79152b87165ecee6d5d7db589a559
Michael Mayes <m0m0sxr@homeoffice.wal-mart.com>
2021-12-10 16:45:05 -0600

    last commit for 12/10

--- src/main/java/com/xpanxion/assignments/student6/JavaThree.java
+++ src/main/java/com/xpanxion/assignments/student6/JavaThree.java
@@ -1,5 +1,7 @@
 package com.xpanxion.assignments.student6;
 
+import com.xpanxion.assignments.instructor.CalculatorException;
+
 public class JavaThree {
 
     public void ex1() throws DivByZeroException {
@@ -12,5 +14,19 @@ public class JavaThree {
             String message = "Cannot divide by zero!";
             throw new DivByZeroException(message);
         }
+    }
+
+
+    public void ex2() {
+        var calculator = new Calculator();
+        try {
+            var result = calculator.calculate(10, 0, "div");
+        } catch (DivByZeroException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void ex3() {
+
     }
 }
--- src/main/java/com/xpanxion/assignments/student6/Main.java
+++ src/main/java/com/xpanxion/assignments/student6/Main.java
@@ -20,7 +20,7 @@ public class Main {
         //javaTwo.ex3();
         //javaTwo.ex4();
         //javaTwo.ex5();
-        javaTwo.ex6();
+        //javaTwo.ex6();
         //javaTwo.ex7();
         //javaTwo.ex8();
         //javaTwo.ex9();
@@ -29,5 +29,6 @@ public class Main {
 
         JavaThree javaThree = new JavaThree();
         //javaThree.ex1();
+        javaThree.ex2();
     }
 }
