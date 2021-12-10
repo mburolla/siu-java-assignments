a48ca98715f2a5e3041e645d4d205686b90af397
EMANUEL GARCIA <e0g0186@HOMEOFFICE.wal-mart.com>
2021-12-10 14:58:47 -0600

    Update

--- src/main/java/com/xpanxion/assignments/student9/JavaThree.java
+++ src/main/java/com/xpanxion/assignments/student9/JavaThree.java
@@ -106,7 +106,45 @@ public class JavaThree {
     }
 
     public void ex8() {
+/*        @Test
+        public void shouldPerformAdd(){
+            // Arrange
+            var addCalculator = new Calculator();
+            // Act
+            addCalculator.setOperation("add");
+            //Assert
+            Assertions.assertTrue(addCalculator.isOperationAdd());
+        }
 
+        @Test
+        public void shouldPerformSub(){
+            // Arrange
+            var subCalculator = new Calculator();
+            // Act
+            subCalculator.setOperation("sub");
+            //Assert
+            Assertions.assertTrue(subCalculator.isOperationSub());
+        }
+
+        @Test
+        public void shouldPerformMul(){
+            // Arrange
+            var mulCalculator = new Calculator();
+            // Act
+            mulCalculator.setOperation("mul");
+            //Assert
+            Assertions.assertTrue(mulCalculator.isOperationMul());
+        }
+
+        @Test
+        public void shouldPerformDiv(){
+            // Arrange
+            var divCalculator = new Calculator();
+            // Act
+            divCalculator.setOperation("div");
+            //Assert
+            Assertions.assertTrue(divCalculator.isOperationDiv());
+        }*/
     }
 
 
