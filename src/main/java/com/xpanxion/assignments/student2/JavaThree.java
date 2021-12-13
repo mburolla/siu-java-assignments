d284c39950147b9b5c471980d73a531523cb360b
Barrett Johnson <b0j04cn@homeoffice.wal-mart.com>
2021-12-13 09:19:21 -0600

    Finished ex.1 (Java3)

---
+++ src/main/java/com/xpanxion/assignments/student2/JavaThree.java
@@ -0,0 +1,21 @@
+package com.xpanxion.assignments.student2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class JavaThree {
+
+    public static void ex1() {
+
+        try {
+            var result = 10 / 0;
+        }
+        catch (Exception e) {
+            System.out.println("Cannot divide by zero.");
+        }
+
+    }
+
+
+}
