c5f779e3eec7269701d0a6bb4d789378c512e0e6
Meg <helloiammegg@gmail.com>
2021-12-13 14:33:15 -0600

    finished ex3 of Java Three

--- src/main/java/com/xpanxion/assignments/student8/JavaThree.java
+++ src/main/java/com/xpanxion/assignments/student8/JavaThree.java
@@ -1,5 +1,8 @@
 package com.xpanxion.assignments.student8;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class JavaThree {
@@ -21,6 +24,21 @@ public class JavaThree {
     public void ex2() throws CalculatorException {
         var calculator = new Calculator();
         var result = calculator.calculate(10, 0, "div");
+    }
+
+    public void ex3() {
+        try {
+            BufferedReader br = new BufferedReader(
+                    new FileReader("input-1.txt")
+            );
+            String s;
+            while((s = br.readLine()) != null) {
+                System.out.println(s);
+            }
+            br.close();
+        } catch (Exception e) {
+            System.out.println(e);;
+        }
     }
 
 }
