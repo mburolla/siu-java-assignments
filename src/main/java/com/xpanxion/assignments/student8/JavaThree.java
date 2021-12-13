068e87d6d22762139354458d30129227b0220aaf
Meg <helloiammegg@gmail.com>
2021-12-13 15:14:56 -0600

    finished ex4 of Java Three

--- src/main/java/com/xpanxion/assignments/student8/JavaThree.java
+++ src/main/java/com/xpanxion/assignments/student8/JavaThree.java
@@ -1,9 +1,9 @@
 package com.xpanxion.assignments.student8;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class JavaThree {
 
@@ -38,6 +38,25 @@ public class JavaThree {
             br.close();
         } catch (Exception e) {
             System.out.println(e);;
+        }
+    }
+
+    public void ex4() {
+        try {
+            File file = new File("input-2.txt");
+            ArrayList<Person> personsFromFile = new ArrayList<>();
+            Scanner fileScanner = new Scanner(file);
+            while(fileScanner.hasNextLine()) {
+                StringTokenizer st = new StringTokenizer(fileScanner.nextLine());
+                int id = Integer.parseInt(st.nextToken().replace(",", ""));
+                String firstName = st.nextToken();
+                String lastName = st.nextToken();
+                personsFromFile.add(new Person(id, firstName, lastName));
+            }
+            fileScanner.close();
+            personsFromFile.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 
--- src/main/java/com/xpanxion/assignments/student8/Main.java
+++ src/main/java/com/xpanxion/assignments/student8/Main.java
@@ -29,8 +29,8 @@ public class Main {
         JavaThree javaThree = new JavaThree();
 //        javaThree.ex1();
 //        javaThree.ex2();
-        javaThree.ex3();
-//        javaThree.ex4();
+//        javaThree.ex3();
+        javaThree.ex4();
 //        javaThree.ex5();
 //        javaThree.ex6();
 //        javaThree.ex7();
